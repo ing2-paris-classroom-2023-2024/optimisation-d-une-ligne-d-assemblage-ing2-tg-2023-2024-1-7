1b8f7619864d8d95b9ad0ea7e00519c74829b3b2
Lucas <lucas.schwindenhammer@edu.ece.fr>
2023-11-19 22:21:50 +0100

    Implémentation de la lecture du fichier des précédences dans le format imposé. Et chargement des données dans un graphes orienté avec détermination automatique de la taille et de l'ordre. Le graphe est chargé dans le format proposé lors du TP2.

---
+++ header.h
@@ -0,0 +1,40 @@
+//
+// Created by lucas on 19/11/2023.
+//
+
+#ifndef OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_1_7_HEADER_H
+#define OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_1_7_HEADER_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//liste chainé d'arc
+struct arc{
+
+    int sommet;
+    int poids;
+    struct arc* arc_suivant;
+
+};
+typedef struct arc t_arc;
+
+//structure sommet
+typedef struct Sommet{
+
+    int valeur;
+    int marque;
+    t_arc *arc;
+
+}t_sommet;
+
+//structure graphe
+typedef struct graphe{
+
+    int ordre;
+    int taille;
+    t_sommet* sommet; //tableau dynamique de sommet
+
+}t_graphe;
+
+#endif //OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_1_7_HEADER_H
--- main.c
+++ main.c
@@ -2,8 +2,146 @@
 // Created by lucas on 19/11/2023.
 //
 
-#include <stdio.h>
+#include "header.h"
+
+//création des sommets
+void creerSommet (t_graphe *grf, FILE *fichier){
+
+    int S1 = 0, ordre = 0, marque = 1, comptage = 0;
+    grf->sommet = (t_sommet*)malloc((ordre+1)* sizeof(t_sommet));
+
+    while (fscanf(fichier, "%d", &S1)==1){
+
+        for(int i = 0; i<=ordre; i++){
+            if(grf->sommet[i].valeur == S1){
+                marque = 0;
+            }
+        }
+        if(marque==1){
+            ordre++;
+            grf->sommet = (t_sommet*)realloc(grf->sommet, (ordre+1) * sizeof(t_sommet));
+            grf->sommet[comptage].valeur = S1;
+            grf->sommet[comptage].arc = NULL;
+            comptage++;
+        }
+        marque = 1;
+    }
+    grf->ordre = ordre;
+}
+
+// Initialisation des arrêtes
+void creerArrete(t_graphe *grf, FILE *fichier) {
+
+    //on pourrait rajouter la possibilité de choisir entre graphe orienté ou non
+    //Ici le code permet de charger un graphe non orienté
+    int i = 0, j = 0;
+    int s1 = 0, s2 = 0;
+
+    while (fscanf(fichier, "%d %d", &s1, &s2)==2){// répétition du nombre d'arc présent
+
+        j=0;
+        // Création d'une nouvelle arête de s1 à s2
+        t_arc *new_arc_s1 = malloc(sizeof(t_arc));
+        new_arc_s1->sommet = s2; // sommet de destination de l'arc
+        new_arc_s1->poids = 0; // poids de l'arc
+        new_arc_s1->arc_suivant = NULL;
+
+        // Création d'une nouvelle arête de s2 à s1 (pour les graphe non orienté)
+        /*t_arc *new_arc_s2 = malloc(sizeof(t_arc));
+        new_arc_s2->sommet = s1;
+        new_arc_s2->poids = p;
+        new_arc_s2->arc_suivant = NULL;*/
+        for (int j = 0; s1 != grf->sommet[j].valeur; j++ ){
+
+        }
+
+        //recherche de l'indice du sommet s1
+        while (s1 != grf->sommet[j].valeur){
+            j++;
+        }
+
+        //On ajoute l'arête de S1 à S2 dans le sommet S1
+        if (grf->sommet[j].arc == NULL) { // s'il n'y a pas d'arc
+            grf->sommet[j].arc = new_arc_s1;
+        } else { // sinon on va jusqu'au dernier arc
+            t_arc *arc_temporaire_s1 = grf->sommet[j].arc; // créer un arc temporaire permet de ne pas écraser les arcs déjà présent
+            while (arc_temporaire_s1->arc_suivant != NULL) {
+                arc_temporaire_s1 = arc_temporaire_s1->arc_suivant;
+            }
+            arc_temporaire_s1->arc_suivant = new_arc_s1;
+        }
+
+        i++;
+
+        /* Uniquement pour les graphe non orienté
+        //On ajoute l'arête de S2 à S1 dans le sommet S2
+        if (grf->sommet[s2].arc == NULL) { // s'il n'y a pas d'arc
+            grf->sommet[s2].arc = new_arc_s2;
+        } else { // sinon on va jusqu'au dernier arc
+            t_arc *arc_temporaire_s2 = grf->sommet[s2].arc;
+            while (arc_temporaire_s2->arc_suivant != NULL) {
+                arc_temporaire_s2 = arc_temporaire_s2->arc_suivant;
+            }
+            arc_temporaire_s2->arc_suivant = new_arc_s2;
+        }
+        */
+    }
+    grf->taille=i;
+}
+
+void init_graphe (char *nomfichier, t_graphe *grf)
+{
+    FILE *fichier = fopen(nomfichier,"r");
+    if(!fichier){
+        printf("Erreur de lecture du fichier\n");
+        exit(-1);
+    }
+
+    //création et initialisation des sommets
+    fseek(fichier, 0, SEEK_SET); // on retourne au début du fichier
+    creerSommet(grf, fichier);
+
+    //création et initialisation des arrêtes
+    fseek(fichier, 0, SEEK_SET); // on retourne au début du fichier
+    creerArrete(grf, fichier);
+}
+
+void afficher_graphe(t_graphe grf) {
+    printf("Ordre : %d \n", grf.ordre);
+    printf("Sommets : \n");
+    for (int i = 0; i < grf.ordre; i++) {
+        printf("%d ", grf.sommet[i].valeur);
+    }
+
+    printf("\n");
+    printf("Taille : %d \n", grf.taille);
+    printf("Arretes : \n");
+    for (int i = 0; i < grf.ordre; i++) {
+        t_arc *arc_actuel = grf.sommet[i].arc;
+
+        while (arc_actuel != NULL) {
+            printf("%d %d %d\n", grf.sommet[i].valeur, arc_actuel->sommet, arc_actuel->poids);
+            arc_actuel = arc_actuel->arc_suivant;
+        }
+    }
+}
 
 int main(){
-    printf("Bonjour\n");
+
+    t_graphe precedence; // création du graphe 1
+
+    //récupération du nom du fichier
+    char recip_nom[50];
+    char *nom;
+    printf("Rentrez le nom du fichier à lire :\n");
+    gets(recip_nom);
+    nom = malloc(strlen(recip_nom)+1 * sizeof(char *));
+    strcpy(nom, recip_nom);
+    fflush(NULL);
+
+    //initialisation du graphe
+    init_graphe(nom, &precedence);
+
+    //affichage
+    afficher_graphe(precedence);
 }
