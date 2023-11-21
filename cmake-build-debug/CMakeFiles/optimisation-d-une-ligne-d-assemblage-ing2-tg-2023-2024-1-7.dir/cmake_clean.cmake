ce0df05d2e370bf213a165e964f55d792ade4293
elisha bajemon <elishabajemon60@icloud.com>
2023-11-21 10:01:47 +0100

    test explication

---
+++ cmake-build-debug/CMakeFiles/optimisation-d-une-ligne-d-assemblage-ing2-tg-2023-2024-1-7.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/optimisation-d-une-ligne-d-assemblage-ing2-tg-2023-2024-1-7.dir/main.c.o"
+  "CMakeFiles/optimisation-d-une-ligne-d-assemblage-ing2-tg-2023-2024-1-7.dir/main.c.o.d"
+  "optimisation-d-une-ligne-d-assemblage-ing2-tg-2023-2024-1-7"
+  "optimisation-d-une-ligne-d-assemblage-ing2-tg-2023-2024-1-7.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/optimisation-d-une-ligne-d-assemblage-ing2-tg-2023-2024-1-7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/optimisation-d-une-ligne-d-assemblage-ing2-tg-2023-2024-1-7.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for optimisation-d-une-ligne-d-assemblage-ing2-tg-2023-2024-1-7.
+# This may be replaced when dependencies are built.
